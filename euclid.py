1b5d9d5d5e04349e2e24b757e878d6bbae87739d
theicemarket <42557957+theicemarket@users.noreply.github.com>
2018-08-21 00:01:59 -0400

    Update

--- euclid.py
+++ euclid.py
@@ -1,12 +1,12 @@
 while True:
     a = int(input("Enter a number: "))
     b = int(input("Enter another number: "))
-    c = 0
-    a_1 = a
+    c = 0   #see while loop
+    a_1 = a #saves inputs for the final print message
     b_1 = b
 
     while b != 0:
-        c = a
+        c = a   #saves the value of a before its assignment to b
         a = b
         b = c%b
         print(a,b)
