fd4a795a19e2939879496fe86787ef8a8d3f2e19
theicemarket <42557957+theicemarket@users.noreply.github.com>
2018-08-25 18:18:04 -0400

    Renam

--- euclid.py
+++ euclid.py
@@ -1,35 +1,39 @@
-while True:
-    a = int(input("Enter a number: "))
-    b = int(input("Enter another number: "))
-    c = 0
-    q = 0
-    output_1 = a     #saves a and b for the final print message
-    output_2 = b
-    n_1 = []
+def gcd(a,b):
+    a_list =[]
+    b_list =[]
+    remainder_list = []
+    quotent_list = []
+    while b!=0:
+        a_list.append(a)
+        b_list.append(b)
+        c = b
+        b = a%b
+        remainder_list.append(b)
+        q = a//c
+        quotent_list.append(q)
+        a = c
+    return(a_list, b_list, quotent_list, remainder_list)
 
-#calculates GCD
-    while b != 0:
-        q = a//b    #quotent
-        c = a       #saves the value of a before its assignment to b
-        a = b       #reassigns b to a for next iteration
-        b = c%b     #remainder calculated and assigned to b for next iteration
-        n_1.append(q)   #adds quotients to list
-        print(c, "=", a, "(", q, ")+", b, sep='')    #a = b*(a//b)+a%b
-
-    print("\nGCD(", output_1, ",",  output_2, ")=", a, sep='')
-
-#calculates linear solution
-    n = n_1[::-1]   #reverses list
-    i = 2
+def linear_solution(a,b):
+    coef_lists = gcd(a,b)
+    for i in coef_lists:
+        i.reverse()
     x_1=1
-    y_1=n[1]
-    x_2=0
-
-    while i < len(n):
-        y = n[i]
-        x_2 = (-1)*y_1
-        y_2 = (x_1+y*(y_1))*(-1)
+    y_1=coef_lists[2][1]
+    for i in range(2, len(coef_lists[2])):
+        y = coef_lists[2][i]
+        x_2 = -y_1
+        y_2 = -x_1-y*(y_1)
         x_1 = x_2
         y_1 = y_2
-        i = i + 1
-    print(output_1, "(", x_2, ")+", output_2, "(", -y_2, ")=", a, "\n", sep='')
+    return(x_1,-y_1)
+
+def main():
+    print("\nGCD and linear diopantine solver")
+    a = int(input("Enter a number: \n"))
+    b = int(input("Enter another: \n"))
+    print("GCD = ", gcd(a,b)[2][1])
+    solutions = linear_solution(a,b)
+    print(a, "(", solutions[0], ")+", b, "(", solutions[1],") = 1", sep='')
+
+main()
---
+++ euclid_old.py
@@ -0,0 +1,35 @@
+while True:
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    c = 0
+    q = 0
+    output_1 = a     #saves a and b for the final print message
+    output_2 = b
+    n_1 = []
+
+#calculates GCD
+    while b != 0:
+        q = a//b    #quotent
+        c = a       #saves the value of a before its assignment to b
+        a = b       #reassigns b to a for next iteration
+        b = c%b     #remainder calculated and assigned to b for next iteration
+        n_1.append(q)   #adds quotients to list
+        print(c, "=", a, "(", q, ")+", b, sep='')    #a = b*(a//b)+a%b
+
+    print("\nGCD(", output_1, ",",  output_2, ")=", a, sep='')
+
+#calculates linear solution
+    n = n_1[::-1]   #reverses list
+    i = 2
+    x_1=1
+    y_1=n[1]
+    x_2=0
+
+    while i < len(n):
+        y = n[i]
+        x_2 = (-1)*y_1
+        y_2 = (x_1+y*(y_1))*(-1)
+        x_1 = x_2
+        y_1 = y_2
+        i = i + 1
+    print(output_1, "(", x_2, ")+", output_2, "(", -y_2, ")=", a, "\n", sep='')
