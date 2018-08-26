b8e1538e757f3d3ce754d0225f8ee29a3dff3dd6
theicemarket <42557957+theicemarket@users.noreply.github.com>
2018-08-25 23:17:38 -0400

    Fat fix

--- euclid.py
+++ euclid.py
@@ -12,11 +12,11 @@ def gcd(a,b):
         q = a//c
         quotent_list.append(q)
         a = c
-    return(a_list, b_list, quotent_list, remainder_list)
+    return(a_list, b_list, quotent_list, remainder_list, c)
 
 def linear_solution(a,b):
     coef_lists = gcd(a,b)
-    for i in coef_lists:
+    for i in coef_lists[:-1]:
         i.reverse()
     x_1=1
     y_1=coef_lists[2][1]
@@ -32,8 +32,9 @@ def main():
     print("\nGCD and linear diopantine solver")
     a = int(input("Enter a number: \n"))
     b = int(input("Enter another: \n"))
-    print("GCD = ", gcd(a,b)[2][1])
+    c = gcd(a,b)[4]
+    print(c)
     solutions = linear_solution(a,b)
-    print(a, "(", solutions[0], ")+", b, "(", solutions[1],") = 1", sep='')
+    print(a, "(", solutions[0], ")+", b, "(", solutions[1],") = ",c, sep='')
 
 main()
