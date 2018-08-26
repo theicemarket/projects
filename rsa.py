3d579188374c073a4f78221e2f54cf14bf1cca35
theicemarket <42557957+theicemarket@users.noreply.github.com>
2018-08-25 23:25:36 -0400

    Fix

--- rsa.py
+++ rsa.py
@@ -1,3 +1,6 @@
+import random
+import math
+
 #calculates gcd + linear coefficents
 def gcd(a,b):
     a_list =[]
@@ -13,17 +16,17 @@ def gcd(a,b):
         q = a//c
         quotent_list.append(q)
         a = c
-    return(a_list, b_list, quotent_list, remainder_list)
+    return(a_list, b_list, quotent_list, remainder_list,c)    #gcd = c= gcd(a,b)[4]
 
 #finds x,y s.t. ax+by=gcd(a,b)
-def linear_solution(a,b):
+def linear_solution(a,b,c):
     coef_lists = gcd(a,b)
-    for i in coef_lists:
+    for i in coef_lists[:-1]:   #removes int c from list
         i.reverse()
 
     #seeds
     x_1=1
-    y_1=coef_lists[2][1] # = gcd(a,b)
+    y_1=coef_lists[2][1] # = gcd(a,b)[4] = actual gcd(a,b)
 
     #recursion on a-by = r
     #into c*x-d*y = gcd(a,b) where d = r
@@ -34,28 +37,56 @@ def linear_solution(a,b):
         y_2 = -x_1-y*y_1
         x_1 = x_2
         y_1 = y_2
+    #solution is (x,y) = (x_1, -y_1)
+
+    if c == 1:
+        #forces x_1 to be a positive solution to a(x_1)+(b)(y*)=gcd(a,b)
+        k = 0
+        while x_1 < 0:
+            k = k + 1
+            x_1 = x_1 + k*b
     return(x_1,-y_1)
-    #x_1 is a possibly negative solution to ax+by = 1
+
+def prime_gen():
+    x = 10**140
+    y = 10**200
+    i = 0
+    pseu_primes = []
+    while i != 2:
+        a = random.randint(x,y)
+        b = 1361
+        if pow(b,a-1,a) == 1:
+            pseu_primes.append(a)
+            i = i+1
+    return(pseu_primes)
 
 #keygen
 def keygen():
-    z = int(input("Generate all keys: 0\nFind decryption key given (e,p,q): 1\n"))
+    z = int(input("Generate all keys: 0\nFind decryption key and modulus given (e,p,q): 1\n"))
 
     if z == 0:
-        pass
+        while z == 0:
+            print("Generaring pseudoprimes, please wait: ")
+            pseu_primes = prime_gen()
+            prime_1 = pseu_primes[0]
+            prime_2 = pseu_primes[1]
+            print("Prime 1:", prime_1, "\n\nPrime 2:", prime_2)
+            z = int(input("If values are prime, return 1. \nOtherwise return 0.\n "))
+
+        encrypt = 65537
+        if gcd(encrypt, (prime_1 -1)*(prime_2 - 1))[4] != 1:
+            print("WHAT")
+        decrypt = linear_solution(encrypt,(prime_1 -1)*(prime_2 - 1), 1)[0]
+        print("\nEncryption key: ", encrypt)
+        print("\nDecryption key: ", decrypt, "\n")
+
+
 
     if z == 1:
         encrypt = int(input("\nEnter an encyption key: "))
         prime_1 = int(input("\nEnter first private key: \n"))
         prime_2 = int(input("\nEnter second private key: \n"))
-        decrypt = linear_solution(encrypt,(prime_1 -1)*(prime_2 - 1))[0]
-
-        #forces decrypt to be a positive solution to (encrypt)(decrypt)+(prime_1-1)(prime_2-1)y=1
-        k = 0
-        while decrypt < 0:
-            k = k + 1
-            decrypt = decrypt + k*(prime_1-1)*(prime_2-1)
-        print("Decryption key:", decrypt)
+        decrypt = linear_solution(encrypt,(prime_1 -1)*(prime_2 - 1), 1)[0]
 
 #encryption
 def encryption():
@@ -89,7 +120,7 @@ def decryption():
         p = m.decode("utf-8")
         print(p)
 
-#main
+#main select
 def main():
     print("\n//RSA encryption, decryption, and keygen//")
     w = int(input("Encrypt: 0 \nDecrypt: 1\nKeygen:  2\n\n"))
